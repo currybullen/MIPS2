6f12fdf117c07137dfb41a42dd488145ea44cbd0
Mikael Karlsson <mikaelhakarlsson@gmail.com>
2014-12-09 15:53:17 +0100

    Added an ALUCodes class.

---
+++ src/model/ALUCodes.java
@@ -0,0 +1,13 @@
+package model;
+
+/**
+ * Created by c12mkn on 2014-12-09.
+ */
+public class ALUCodes {
+    public static final byte AND = 0;
+    public static final byte OR = 1;
+    public static final byte ADD = 2;
+    public static final byte SUBTRACT = 6;
+    public static final byte SETONLESSTHAN = 7;
+    public static final byte NOR = 12;
+}
