bbd4438da1ea3859ba598a59289b9ac80a558cb0
Mikael Karlsson <mikaelhakarlsson@gmail.com>
2014-12-10 10:43:19 +0100

    Changed some minor code in the MIPS2 class.

--- src/controller/MIPS2.java
+++ src/controller/MIPS2.java
@@ -3,12 +3,16 @@ package controller;
 import model.InstructionMemory;
 import model.InstructionMemory;
 import model.RegisterLabels;
+import model.Registers;
+
+import java.util.Random;
 
 /**
  * Created by currybullen on 2014-12-06.
  */
 public class MIPS2 {
     private InstructionMemory instructionMemory;
+    private Registers registers;
 
     public MIPS2(String fileName) {
         InstructionParser instructionParser = new InstructionParser(new
@@ -21,16 +25,38 @@ public class MIPS2 {
     }
 
     private void go() {
-        testPrint();
+        registers = new Registers();
+        testInitializeRegisters();
+        testPrintRegisters();
+        //testPrint();
     }
 
     private void testPrint() {
         for (int i = 0; i < instructionMemory.size(); i++) {
-            String mnemonic = instructionMemory.getInstruction(i).getMnemonic();
-            String decomposed = instructionMemory.getInstruction(i).
+            String mnemonic = instructionMemory.getInstruction(4*i).
+                    getMnemonic();
+            String decomposed = instructionMemory.getInstruction(4*i).
                     getDecomposedString();
             System.out.println("Mnemonic: " + mnemonic + " " + "Decomposed: " +
                     decomposed);
+        }
+    }
+
+    private void testPrintRegisters() {
+        for (int i = 0; i < 32; i++) {
+            System.out.println("Register " + i + ": " + registers.
+                    getRegister(i));
+        }
+    }
+
+    private void testInitializeRegisters() {
+        Random random = new Random();
+        for(int i = 0; i < 32; i++) {
+            int randomNumber = random.nextInt(Integer.MAX_VALUE);
+            if (random.nextInt(2) == 1) {
+                randomNumber = -randomNumber;
+            }
+            registers.setRegister(i, randomNumber);
         }
     }
 }
